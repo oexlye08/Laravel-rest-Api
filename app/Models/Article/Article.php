12b91854ca9842b7cb8aa4c5c6d48fdcc7da9d58
Oki Sulton Alimi <os.alimie@gmail.com>
2020-08-04 14:03:33 +0700

    add tutorial

--- app/Models/Article/Article.php
+++ app/Models/Article/Article.php
@@ -21,6 +21,7 @@ class Article extends Model
     {
         return $this->belongsTo(User::class);
     }
+    
     public function subject()
     {
         return $this->belongsTo(Subject::class);
